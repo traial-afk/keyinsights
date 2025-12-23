3bd49dee44e72a499132722aeb44890697007a67
traial-afk <nithinsripathi93@gmail.com>
2025-12-23 11:25:17 -0500

    Change lastModified to current date in sitemap
    Update lastModified to use the current date instead of post date.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,7 +7,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
     const blogUrls = posts.map((post) => ({
         url: `${baseUrl}/blog/${post.slug}`,
-        lastModified: new Date(post.date),
+        lastModified: new Date(),
         changeFrequency: 'weekly' as const,
         priority: 0.8,
     }))
