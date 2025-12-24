3f6b72056f2d8642493a1165c7bd1cf69899edc7
traial-afk <nithinsripathi93@gmail.com>
2025-12-24 09:17:31 -0500

    Integrate Google Tag Manager for analytics
    Added Google Tag Manager scripts for tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,6 +65,17 @@ export default function RootLayout({
           <Toaster />
         </ThemeProvider>
         
+           
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RSSZ6B5GWX"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-RSSZ6B5GWX');
+       </script>
+        
         {/* Microsoft Clarity */}
         <Script id="microsoft-clarity" strategy="afterInteractive">
           {`
